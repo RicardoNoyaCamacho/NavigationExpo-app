55eefecb3b74044e73e700876f158767fdd5f27b
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2025-07-18 12:14:49 -0600

    Add tabs

---
+++ app/tabs/favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const FoforitesScreen = () => {
+    return (
+        <View>
+            <Text>FoforitesScreen</Text>
+        </View>
+    )
+}
+
+export default FoforitesScreen
---
+++ app/tabs/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomIndexScreen = () => {
+    return (
+        <View>
+            <Text>HomIndexScreen</Text>
+        </View>
+    )
+}
+
+export default HomIndexScreen
